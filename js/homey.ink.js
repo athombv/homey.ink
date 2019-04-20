c3a80872b51f3f6f073d2a7c5c99fd98821a3d2e
Emile Nijssen <emile@athom.nl>
2019-04-20 20:12:15 +0200

    fixes

--- js/homey.ink.js
+++ js/homey.ink.js
@@ -1,15 +1,198 @@
+var CLIENT_ID = '5cbb504da1fc782009f52e46';
+var CLIENT_SECRET = 'gvhs0gebgir8vz8yo2l0jfb49u9xzzhrkuo1uvs8';
+var REDIRECT_URL = 'http://localhost:5000';
+
 window.addEventListener('load', function() {
   
+  var homey;
+  var me;
+  
+  var $text = document.getElementById('text');
   var $logo = document.getElementById('logo');
+  var $flowsInner = document.getElementById('flows-inner');
+  var $devicesInner = document.getElementById('devices-inner');
+  
   $logo.addEventListener('click', function(){
     window.location.reload();
   });
   
-  var $test = document.getElementById('test');
-  $test.addEventListener('click', function(){
-    window.location.reload();
+  var api = new AthomCloudAPI({
+    clientId: CLIENT_ID,
+    clientSecret: CLIENT_SECRET,
+    redirectUrl: REDIRECT_URL,
   });
   
-  var $text = document.getElementById('text');
+  var token = getQueryVariable('token');
+  token = atob(token);
+  token = JSON.parse(token);
+  api.setToken(token);
+  
+  api.isLoggedIn().then(function(loggedIn) {
+    if(!loggedIn)
+      throw new Error('Token Expired. Please log-in again.');
+  }).then(function(){
+    return api.getAuthenticatedUser();
+  }).then(function(user) {
+    return user.getFirstHomey();
+  }).then(function(homey) {
+    return homey.authenticate();
+  }).then(function(homey_) {
+    homey = homey_;
+    console.log('homey', homey)
+        
+    return homey.users.getUserMe().then(function(user) {
+      me = user;
+      me.properties = me.properties || {};
+      me.properties.favoriteFlows = me.properties.favoriteFlows || [];
+      me.properties.favoriteDevices = me.properties.favoriteDevices || [];
+      
+      homey.flow.getFlows().then(function(flows) {
+        var favoriteFlows = me.properties.favoriteFlows.map(function(flowId){
+          return flows[flowId];
+        }).filter(function(flow){
+          return !!flow;
+        });
+        return renderFlows(favoriteFlows);        
+      }).catch(document.write);
+      
+      homey.devices.getDevices().then(function(devices) {
+        var favoriteDevices = me.properties.favoriteDevices.map(function(deviceId){
+          return devices[deviceId];
+        }).filter(function(device){
+          return !!device;
+        }).filter(function(device){
+          if(!device.ui) return false;
+          if(device.ui.quickAction !== 'onoff') return false;          
+          return true;
+        });
+        return renderDevices(favoriteDevices);
+      }).catch(function(error){
+        document.write(error);
+      });
+    });
+    
+  }).catch(function(err) {
+    console.error(err);
+    document.write(err);
+  });
+  
+  function renderFlows(flows) {
+    $flowsInner.innerHTML = '';
+    flows.forEach(function(flow) {
+      var $flow = document.createElement('div');
+      $flow.classList.add('flow');
+      $flow.addEventListener('click', function(){
+        homey.flow.triggerFlow({
+          id: flow.id,
+        }).catch(function(error){
+          document.write(error);
+        });
+      });
+      $flowsInner.appendChild($flow);
+      
+      var $play = document.createElement('div');
+      $play.classList.add('play');
+      $flow.appendChild($play);
+      
+      var $name = document.createElement('div');
+      $name.classList.add('name');
+      $name.innerHTML = flow.name;
+      $flow.appendChild($name);
+    });
+  }
+  
+  function renderDevices(devices) {
+    $devicesInner.innerHTML = '';
+    devices.forEach(function(device) {
+      var $device = document.createElement('div');
+      $device.classList.add('device');
+      $device.classList.toggle('on', device.capabilitiesObj && device.capabilitiesObj.onoff && device.capabilitiesObj.onoff.value === true);
+      $device.addEventListener('click', function(){
+        var value = !$device.classList.contains('on');
+        $device.classList.toggle('on', value);
+        homey.devices.setCapabilityValue({
+          deviceId: device.id,
+          capabilityId: 'onoff',
+          value: value,
+        }).catch(function(error){
+          document.write(error);
+        });
+      });
+      $devicesInner.appendChild($device);
+      
+      var $icon = document.createElement('div');
+      $icon.classList.add('icon');
+      $icon.style.webkitMaskImage = 'url(https://icons-cdn.athom.com/' + device.iconObj.id + '-64.png)';
+      $device.appendChild($icon);
+      
+      var $name = document.createElement('div');
+      $name.classList.add('name');
+      $name.innerHTML = device.name;
+      $device.appendChild($name);
+    });
+  }
   
 });
+
+function getQueryVariable(variable) {
+  var query = window.location.search.substring(1);
+  var vars = query.split('&');
+  for (var i = 0; i < vars.length; i++) {
+      var pair = vars[i].split('=');
+      if (decodeURIComponent(pair[0]) == variable) {
+          return decodeURIComponent(pair[1]);
+      }
+  }
+  console.log('Query variable %s not found', variable);
+}
+
+// Polyfills
+if (!String.prototype.startsWith) {
+	String.prototype.startsWith = function(search, pos) {
+		return this.substr(!pos || pos < 0 ? 0 : +pos, search.length) === search;
+	};
+}
+
+if (!Array.prototype.find) {
+  Object.defineProperty(Array.prototype, 'find', {
+    value: function(predicate) {
+     // 1. Let O be ? ToObject(this value).
+      if (this == null) {
+        throw new TypeError('"this" is null or not defined');
+      }
+
+      var o = Object(this);
+
+      // 2. Let len be ? ToLength(? Get(O, "length")).
+      var len = o.length >>> 0;
+
+      // 3. If IsCallable(predicate) is false, throw a TypeError exception.
+      if (typeof predicate !== 'function') {
+        throw new TypeError('predicate must be a function');
+      }
+
+      // 4. If thisArg was supplied, let T be thisArg; else let T be undefined.
+      var thisArg = arguments[1];
+
+      // 5. Let k be 0.
+      var k = 0;
+
+      // 6. Repeat, while k < len
+      while (k < len) {
+        // a. Let Pk be ! ToString(k).
+        // b. Let kValue be ? Get(O, Pk).
+        // c. Let testResult be ToBoolean(? Call(predicate, T, « kValue, k, O »)).
+        // d. If testResult is true, return kValue.
+        var kValue = o[k];
+        if (predicate.call(thisArg, kValue, k, o)) {
+          return kValue;
+        }
+        // e. Increase k by 1.
+        k++;
+      }
+
+      // 7. Return undefined.
+      return undefined;
+    }
+  });
+}
