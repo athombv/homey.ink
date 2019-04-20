31cc19beee82a9425def38be79a9762a5cc6e061
Emile Nijssen <emile@athom.nl>
2019-04-20 18:49:02 +0200

    initial commit

---
+++ js/homey.ink.js
@@ -0,0 +1,15 @@
+window.addEventListener('load', function() {
+  
+  var $logo = document.getElementById('logo');
+  $logo.addEventListener('click', function(){
+    window.location.reload();
+  });
+  
+  var $test = document.getElementById('test');
+  $test.addEventListener('click', function(){
+    window.location.reload();
+  });
+  
+  var $text = document.getElementById('text');
+  
+});
