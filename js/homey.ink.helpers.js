417acd7a3e8bfbf610b6e8a27814845a4aa638d7
Emile Nijssen <emile@athom.nl>
2019-04-26 12:30:23 +0200

    commit message

---
+++ js/homey.ink.helpers.js
@@ -0,0 +1,32 @@
+function getQueryVariable(variable) {
+  var query = window.location.search.substring(1);
+  var vars = query.split('&');
+  for (var i = 0; i < vars.length; i++) {
+      var pair = vars[i].split('=');
+      if (decodeURIComponent(pair[0]) == variable) {
+          return decodeURIComponent(pair[1]);
+      }
+  }
+}
+
+function downloadFile(data, fileName, type="text/plain") {
+  // Create an invisible A element
+  const a = document.createElement("a");
+  a.style.display = "none";
+  document.body.appendChild(a);
+
+  // Set the HREF to a Blob representation of the data to be downloaded
+  a.href = window.URL.createObjectURL(
+    new Blob([data], { type })
+  );
+
+  // Use download attribute to set set desired file name
+  a.setAttribute("download", fileName);
+
+  // Trigger the download by simulating click
+  a.click();
+
+  // Cleanup
+  window.URL.revokeObjectURL(a.href);
+  document.body.removeChild(a);
+}
---
+++ js/homey.ink.js
@@ -0,0 +1,83 @@
+const CLIENT_ID = '5cbb504da1fc782009f52e46';
+const CLIENT_SECRET = 'gvhs0gebgir8vz8yo2l0jfb49u9xzzhrkuo1uvs8';
+const REDIRECT_URL = `${window.location.protocol}//${window.location.host}/login.html`;
+
+window.addEventListener('load', () => {
+  
+  let homepage;
+  let config;
+
+  const api = new AthomCloudAPI({
+    clientId: CLIENT_ID,
+    clientSecret: CLIENT_SECRET,
+    redirectUrl: REDIRECT_URL,
+  });
+  
+  const $setup = document.getElementById('setup');
+  
+  const $setupStepLogin = $setup.querySelector('.setup-step-login');
+  const $setupStepLoginButton = $setupStepLogin.querySelector('.setup-step-button');
+  
+  const $setupStepUpload = $setup.querySelector('.setup-step-upload');
+  const $setupStepUploadButton = $setupStepUpload.querySelector('.setup-step-button');
+  const $setupStepUploadFile = $setupStepUpload.querySelector('#setup-step-upload-file');
+  
+  const $setupStepDownload = $setup.querySelector('.setup-step-download');
+  const $setupStepDownloadButton = $setupStepDownload.querySelector('.setup-step-button');
+  
+  $setupStepLoginButton.addEventListener('click', () => {
+    const url = api.getLoginUrl();
+    const popup = window.open(url, 'login', 'width=500,height=800');
+    
+    window.addEventListener('message', message => {
+      const { code } = message.data;
+      if(!code) return alert('Something went wrong.');
+      
+      api.authenticateWithAuthorizationCode(code).then(token => {
+        token = JSON.stringify(token);
+        token = btoa(token);
+        
+        const url = new URL('https://homey.ink/app/index.html');
+        url.searchParams.append('theme', 'kobo-h2o');
+        url.searchParams.append('token', token);
+        
+        homepage = url.toString();
+        console.log('URL:', homepage);
+        
+        $setupStepLogin.classList.add('completed');
+        $setupStepUpload.classList.remove('disabled');
+      }).catch(err => {
+        alert(err.message || err.toString());
+      });
+    })
+  });
+  
+  $setupStepUploadFile.addEventListener('change', e => {
+    const [ file ] = e.target.files;
+    const reader = new FileReader();
+    reader.readAsText(file, 'UTF-8');
+    reader.onload = e => {
+      config = window.ini.parse(e.target.result);
+      
+      config.Browser = config.Browser || {};
+      config.Browser.homePage = homepage;      
+      
+      config.FeatureSettings = config.FeatureSettings || {};
+      config.FeatureSettings.FullScreenBrowser = true;
+      
+      $setupStepUpload.classList.add('completed');
+      $setupStepDownload.classList.remove('disabled');      
+    }
+    reader.onerror = err => {
+      alert(err.message || err.toString());
+    }
+  });
+  
+  
+  $setupStepDownloadButton.addEventListener('click', () => {
+    const file = ini.stringify(config);
+    downloadFile(file, 'Kobo eReader.conf');
+    $setupStepDownload.classList.add('completed');
+  });
+  
+});
